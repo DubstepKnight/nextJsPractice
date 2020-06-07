e4c66cc4db986f2e14956b39d83d482cbad48e59
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2020-06-07 11:07:49 +0300

    Updated

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import Header from "../components/Header";
+const App = ({ Component, pageProps }) => {
+    return <>
+        <Header />
+        <Component {...pageProps} />
+    </>
+}
+
+export default App;
